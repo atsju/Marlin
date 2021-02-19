eb6b6a41332437e0704b361cb17e8813e7eaed6a
Julien Staub <atsju2@yahoo.fr>
2021-02-19 13:09:34 +0100

    PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -492,9 +492,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  9.5
-    #define DEFAULT_Ki   0.35
-    #define DEFAULT_Kd  64.7
+    #define DEFAULT_Kp  13.2
+    #define DEFAULT_Ki   0.65
+    #define DEFAULT_Kd  67.5
   #endif
 #endif // PIDTEMP
 
